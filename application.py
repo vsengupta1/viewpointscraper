69928ef2a825a76eaa336d9d55937c4b10cb3e06
vsengupta1 <143166062+vsengupta1@users.noreply.github.com>
2024-09-17 14:06:28 -0700

    Update application.py

--- application.py
+++ application.py
@@ -91,4 +91,6 @@ def get_campaign():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 8000))  # Azure sets PORT
+    app.run(host='0.0.0.0', port=port, debug=True)
